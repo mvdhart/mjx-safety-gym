d84784030e33c1710dfe86028bc14ea534104445
Max van der Hart <20668633+mvdhart@users.noreply.github.com>
2025-05-22 13:20:51 +0200

    Testing madrona setup script

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ To run the interactive viewer on MacOS M1, run
 ```sudo mjpython scripts/interactive.py ```
 
 Note that we need sudo privileges to get access to keypress information. 
+
+
+## Madrona
+To use vision-based observations, run 
+```
+chmod +x vision_setup.bash
+./vision_setup.bash
+```
+This requires a linux machine with an NVidia GPU and can take up to 5 minutes to install.
+
---
+++ vision_setup.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Ensure Jax matches the CUDA that will be used to build Madrona MJX
+pip uninstall -y jax
+pip install 'jax[cuda12_local]'==0.4.35
+
+# Install required system dependencies
+sudo apt update
+sudo apt install -y \
+  libx11-dev \
+  libxrandr-dev \
+  libxinerama-dev \
+  libxcursor-dev \
+  libxi-dev \
+  mesa-common-dev
+
+# Prevent Python from trying to import the source packages.
+mkdir -p modules
+cd modules
+
+# -- 7) Install madrona-mjx
+echo "Cloning madrona-mjx..."
+git clone https://github.com/shacklettbp/madrona_mjx.git
+
+cd madrona_mjx
+git submodule update --init --recursive
+
+mkdir build
+cd build
+cmake -DLOAD_VULKAN=OFF ..
+make -j 8
+
+cd ..
+pip install -e .
