c29707f2c9770c3e274d55f7e7df351a96ee1c28
Max van der Hart <20668633+mvdhart@users.noreply.github.com>
2025-06-16 14:35:27 +0200

    Updated madrona dependencies

--- vision_setup.bash
+++ vision_setup.bash
@@ -1,34 +1,38 @@
-#!/bin/bash
+echo "1) Preparing environment" 
+module purge
+module load stack/2024-06
+module load gcc/12.2.0
 
-# Ensure Jax matches the CUDA that will be used to build Madrona MJX
-pip uninstall -y jax
-pip install 'jax[cuda12_local]'==0.4.35
+module load cmake/3.27.7
 
-# Install required system dependencies
-sudo apt update
-sudo apt install -y \
-  libx11-dev \
-  libxrandr-dev \
-  libxinerama-dev \
-  libxcursor-dev \
-  libxi-dev \
-  mesa-common-dev
+# Madrona dependencies 
+module load libx11/1.8.4-ns5x2da
+module load libxrandr/1.5.3-acspwjp
+module load libxinerama/1.1.3
+module load libxcursor/1.2.1
+module load libxi/1.7.6-qeazdpn
+module load mesa/23.0.3
 
-# Prevent Python from trying to import the source packages.
-mkdir -p modules
-cd modules
+# Hidden Madrona dependencies :^) 
+module load libxrender/0.9.10-kss2t7k
+module load libxext/1.3.3-e74gj2z
+module load libxfixes/5.0.2-5fbeidb
 
-# -- 7) Install madrona-mjx
-echo "Cloning madrona-mjx..."
+# Cuda + Python 
+module load python_cuda/3.11.6
+
+echo "2) Installing mujoco in Python environment" 
+pip3 install -U mujoco mujoco-mjx
+
+echo "3) Installing Madrona" 
 git clone https://github.com/shacklettbp/madrona_mjx.git
-
 cd madrona_mjx
 git submodule update --init --recursive
-
 mkdir build
 cd build
-cmake -DLOAD_VULKAN=OFF ..
-make -j 8
-
+cmake ..
+make -j
 cd ..
-pip install -e .
+pip3 install -e .
+
+echo "4) Installation Done - check by running 'from madrona_mjx import BatchRenderer' in a python shell"
